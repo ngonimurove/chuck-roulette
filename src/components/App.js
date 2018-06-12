b4f17c5f01ca0b96e6522a1a8e6c21135d5768d7
Taps Murove <taps.murove@gmail.com>
2018-06-12 16:52:06 +0200

    Added basic html and css
    - added basic site mockup with html and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
-import logo from "../assets/images/logo.svg";
+import chuck from "../assets/images/chuck-machine-guns-web.png";
 import "../assets/css/App.css";
+import ErrorBoundary from "./ErrorBoundary";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ErrorBoundary>
+          <header className="header">
+            <h1 className="title">Chuck-Roulette</h1>
+            <img src={chuck} className="header-image" alt="chuck" />
+          </header>
+          <main>
+            <div className="joke">
+              <p>
+                Chuck Norris is the problem to all the answers to your problems!
+              </p>
+            </div>
+            <div className="roulette">
+              <button className="roulette-button">HIT ME!</button>
+              <p className="instructions">
+                Click on “Hit me!” to get a random Chuck Norris joke, or select
+                one of the categories below.
+              </p>
+              <div className="categories">
+                <button className="joke-category">Explicit</button>
+                <button className="joke-category">Dev</button>
+                <button className="joke-category">Movie</button>
+                <button className="joke-category">Food</button>
+                <button className="joke-category">Celebrity</button>
+                <button className="joke-category">Science</button>
+                <button className="joke-category">Sport</button>
+                <button className="joke-category">Political</button>
+                <button className="joke-category">Religion</button>
+                <button className="joke-category">Animal</button>
+                <button className="joke-category">Music</button>
+                <button className="joke-category">Travel</button>
+                <button className="joke-category">Career</button>
+                <button className="joke-category">Money</button>
+                <button className="joke-category">Fashion</button>
+              </div>
+            </div>
+          </main>
+        </ErrorBoundary>
       </div>
     );
   }
---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      errorInfo: null
+    };
+  }
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ error: error, errorInfo: info });
+    // Log the error to an error reporting service
+    // logErrorToMyService(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render fallback UI
+      return <h1>Sorry, looks like Chuck broke the internet.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
